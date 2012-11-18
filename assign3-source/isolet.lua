e42e159a3f176b27461800d37c45e8a62a08a313
Praveen Thirukonda <pt795@nyu.edu>
2012-11-18 17:19:58 -0500

    working on isolet.lua

--- assign3-source/isolet.lua
+++ assign3-source/isolet.lua
@@ -1,5 +1,16 @@
 --[[
 Torch/Lua file which will be used for retrieving data from isolet dataset and forming train and test data objects from the dataset.
-]]--
+]]
+
+-- the spambase dataset
+isolet = {};
+
+-- The dataset has 4601 rows (observations) 
+function isolet:size() return 4601 end
+
+-- Each row (observaton) has 617 features
+function isolet:features() return 617 end
+
+
 
 
