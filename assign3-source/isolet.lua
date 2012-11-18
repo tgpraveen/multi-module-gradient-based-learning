95a8acaa1768b11bbdfc7bbdd04c4a1a6a0b39b7
Praveen Thirukonda <pt795@nyu.edu>
2012-11-18 17:38:32 -0500

    Adding extracted isolet .data files.

--- assign3-source/isolet.lua
+++ assign3-source/isolet.lua
@@ -1,16 +1,16 @@
 --[[
 Torch/Lua file which will be used for retrieving data from isolet dataset and forming train and test data objects from the dataset.
+
+Original dataset from http://archive.ics.uci.edu/ml/datasets/ISOLET
 ]]
 
 -- the spambase dataset
 isolet = {};
 
--- The dataset has 4601 rows (observations) 
-function isolet:size() return 4601 end
+-- The dataset has 7797 rows (given in the dataset's description)
+function isolet:size() return 7797 end
 
 -- Each row (observaton) has 617 features
 function isolet:features() return 617 end
-
-
 
 
