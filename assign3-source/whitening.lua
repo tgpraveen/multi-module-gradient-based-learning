2a654f9d01e55ce1775ff32288c120cd2c46f626
Praveen Thirukonda <pt795@nyu.edu>
2012-11-19 16:47:26 -0500

    Created whitening.lua and started working on it.

--- assign3-source/whitening.lua
+++ assign3-source/whitening.lua
@@ -1,26 +1,58 @@
 function whitenDatasets(train, test, k)
-whitentrain = {}
-whitentest = {}
-sigmasummationpart = {}
-sigmatrain = {}
+	-- whitentrain = {}
+	-- whitentest = {}
+	sigmasummationpart = {}
+	sigmatrain = {}
 
-for i =1, train:size()
-sigmasummationpart = torch.mul(train[i][1],train[i][1]:t())
-end
+	for i =1, train:size()
+		sigmasummationpart = torch.mul(train[i][1],train[i][1]:t())
+	end
 
-sigmatrain = (1/train:size()) * sigmasummationpart
+	sigmatrain = (1/train:size()) * sigmasummationpart
 
-u,s,v = torch.svd(sigmatrain)
+	u,s,v = torch.svd(sigmatrain)
 
-ureduce = {}
+	ureduce = {}
 
-for j = 1,train:features()
-for l = 1,k
-ureduce[j][l] = u[j][l]
-end
-end
+	for j = 1,train:features()
+		for l = 1,k
+			ureduce[j][l] = u[j][l]
+		end
+	end
+
+	for i=1,train:size()
+		train[i][1]=torch.mul(ureduce:t(), train[i][1])
+	end
+    
+    function train:features() return k
+
+	-- Now doing the same thing for testing data set.
+
+    sigmasummationpart2 = {}
+	sigmatest = {}
+
+	for i =1, test:size()
+		sigmasummationpart2 = torch.mul(test[i][1],test[i][1]:t())
+	end
+
+	sigmatest = (1/test:size()) * sigmasummationpart2
+
+	u,s,v = torch.svd(sigmatest)
+
+	ureduce2 = {}
+
+	for j = 1,test:features()
+		for l = 1,k
+			ureduce2[j][l] = u[j][l]
+		end
+	end
+
+	for i=1,test:size()
+		test[i][1]=torch.mul(ureduce2:t(), test[i][1])
+	end
+    
+    function test:features() return k
 
 
-
-return whitenedtrain, whitenedtest
+	return train, test
 end
